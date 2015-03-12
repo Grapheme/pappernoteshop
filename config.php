acc7fc3e8556bdd966d10ad99c96c2341adeb5b6
Владимир <vkharseev@gmail.com>
2015-03-12 18:39:22 +0300

    start

--- config.php
+++ config.php
@@ -20,8 +20,10 @@ define('DIR_LOGS', '/var/www/code/data/www/pappernoteshop.com/system/logs/');
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'pappernoteshop');
-define('DB_PASSWORD', 'XfWfaUcQ5anW6mpZ');
+#define('DB_USERNAME', 'pappernoteshop');
+define('DB_USERNAME', 'root');
+#define('DB_PASSWORD', 'XfWfaUcQ5anW6mpZ');
+define('DB_PASSWORD', 'k.,jdmythtdyetnyfghfcyj');
 define('DB_DATABASE', 'pappernoteshop');
 define('DB_PREFIX', 'oc_');
 ?>
