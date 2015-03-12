795a22f0cd0d90dd9c5889cb6caf706e8a9cb649
Владимир <vkharseev@gmail.com>
2015-03-12 18:20:10 +0300

    start

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://pappernoteshop.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://pappernoteshop.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/code/data/www/pappernoteshop.com/catalog/');
+define('DIR_SYSTEM', '/var/www/code/data/www/pappernoteshop.com/system/');
+define('DIR_DATABASE', '/var/www/code/data/www/pappernoteshop.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/code/data/www/pappernoteshop.com/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/code/data/www/pappernoteshop.com/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/code/data/www/pappernoteshop.com/system/config/');
+define('DIR_IMAGE', '/var/www/code/data/www/pappernoteshop.com/image/');
+define('DIR_CACHE', '/var/www/code/data/www/pappernoteshop.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/code/data/www/pappernoteshop.com/download/');
+define('DIR_LOGS', '/var/www/code/data/www/pappernoteshop.com/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'pappernoteshop');
+define('DB_PASSWORD', 'XfWfaUcQ5anW6mpZ');
+define('DB_DATABASE', 'pappernoteshop');
+define('DB_PREFIX', 'oc_');
+?>
